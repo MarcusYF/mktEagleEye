5754b37a236edb876c4d7b6b05d734220fb645c1
fan.yao <yaofan29597@163.com>
2019-07-01 00:11:12 +0800

    nothing

--- JQData/test.py
+++ JQData/test.py
@@ -1,9 +1,26 @@
+import jqdatasdk as jqd
 from jqdatasdk import *
-import sys, os
+import json
+from os import path
+import os,sys
 
 root = os.getcwd()
 
 file = open(root + '/JQData/Conf.txt', "r")
 Conf = json.load(file)
-auth(Conf['username'], Conf['password'])
-get_price("000001.XSHE", start_date="2017-01-01", end_date="2017-12-31")
+# auth(Conf['username'], Conf['password'])
+
+
+# jqd.get_price("000001.XSHE", start_date="2017-01-01", end_date="2017-12-31")
+
+#查询当日剩余可调用数据条数
+count=get_query_count()
+print(count)
+
+df = get_all_securities(['etf'])
+print(df)
+
+df[df.display_name == '中小板']
+
+
+# https://www.joinquant.com/default/index/sdk
--- scrapeStock.py
+++ scrapeStock.py
@@ -151,23 +151,3 @@ if __name__ == '__main__':
 #
 
 
-import requests,json
-url="https://dataapi.joinquant.com/apis"
-#获取调用凭证
-body={
-    "method": "get_token",
-    "mob": "17801050240",  #mob是申请JQData时所填写的手机号
-    "pwd": "7q5s94rg",  #Password为聚宽官网登录密码，新申请用户默认为手机号后6位
-}
-response = requests.post(url, data = json.dumps(body))
-token=response.text
-#调用get_security_info获取单个标的信息
-body={
-    "method": "get_all_securities",
-    "token": token,
-    "code": "stock",
-}
-response = requests.post(url, data = json.dumps(body))
-print(response.text[:1000])
-
-len(response.text)
