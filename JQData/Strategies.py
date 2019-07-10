aaba677ebcd70c1e206679a26c6649f72b9429c3
fan.yao <yaofan29597@163.com>
2019-07-10 12:12:19 +0800

    add sample strategy

--- JQData/Strategies.py
+++ JQData/Strategies.py
@@ -1,3 +1,4 @@
+import pandas as pd
 
 class tradeManager(object):
 
@@ -21,9 +22,52 @@ class tradeManager(object):
         self.cash += cash_out
         self.avg_price = self.stock_value / self.share
 
+    def print_summary(self, current_value):
+        stock_value = self.share * current_value
+        print("总资产:{:.2f}, 可用资金:{:.2f}, 市值:{:.2f}, 持仓:{:.2f}, 成本:{:.3f}, 现价:{:.3f}, 盈亏:{:.2%}" \
+              .format(self.cash + stock_value, self.cash, \
+                stock_value, self.share, self.avg_price, current_value, (-self.avg_price+current_value) / self.avg_price))
+
 
 def main():
+    t = pd.read_excel("HistoryData_510050.xls")
+    t['收盘价'].values
+
+
     t = tradeManager(100000)
+    t.buy(1, 10000)
+    t.print_summary(1)
+    t.buy(0.9, 11000)
+    t.print_summary(0.9)
+    t.buy(0.8, 12500)
+    t.print_summary(0.8)
+    t.buy(0.7, 14000)
+    t.print_summary(0.7)
+    t.sell(0.8, 14000)
+    t.print_summary(0.8)
+    t.buy(0.7, 14000)
+    t.sell(0.8, 14000)
+    t.buy(0.7, 14000)
+    t.sell(0.8, 14000)
+    t.buy(0.7, 14000)
+    t.sell(0.8, 14000)
+    t.buy(0.7, 14000)
+    t.sell(0.8, 14000)
+    t.buy(0.7, 14000)
+    t.sell(0.8, 14000)
+    t.buy(0.7, 14000)
+    t.sell(0.8, 14000)
+    t.buy(0.7, 14000)
+    t.sell(0.8, 14000)
+    t.buy(0.7, 14000)
+    t.sell(0.8, 14000)
+    t.buy(0.7, 14000)
+    t.sell(0.8, 14000)
+    t.buy(0.7, 14000)
+    t.sell(0.8, 14000)
+    t.buy(0.7, 14000)
+    t.sell(0.8, 14000)
+    t.print_summary(0.77)
 
 
 if __name__ == '__main__':
